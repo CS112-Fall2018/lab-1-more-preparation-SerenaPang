1187fddb2af9d7198f31f94d59ef87d908ec983d
SerenaPang <42624839+SerenaPang@users.noreply.github.com>
2018-08-27 18:39:00 -0700

    Add files via upload

---
+++ NewMath.java
@@ -0,0 +1,19 @@
+
+/** This program performs subtraction and multiplication in the main method */
+
+
+public class NewMath
+{
+    public static void main(String[] args){
+        System.out.println("mutiplication and subtraction for 3 different data type");
+
+        short s = (123 *100)-100;
+        long l = (123456 * 10000) - 200000;
+        double d = (1234.456 * 567.89) - 234.567;
+
+
+        System.out.println(s);
+        System.out.println(l);
+        System.out.println(d);
+    }
+}
