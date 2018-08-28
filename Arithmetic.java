bfbba2ae96e01bbaec5c2f4fd4e213de0be4d2a1
SerenaPang <ypang4@usfca.edu>
2018-08-28 15:08:00 -0700

    lab 1 done

---
+++ Arithmetic.java
@@ -0,0 +1,22 @@
+
+public class Arithmetic {
+
+public static void main(String[] args) {  
+
+	int a = 2;
+	int b = 4;
+
+	float x = 1.2f;
+	float y = 2.4f;
+
+
+System.out.println( a + b);
+System.out.println(a / b);
+System.out.println(x + y);
+System.out.println(x / y);
+
+}
+
+}
+
+
